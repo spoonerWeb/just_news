c68f6cc18f1e00331199bbd2c203c4b2afc1cdb6
Thomas Löffler <loeffler@spooner-web.de>
2016-10-17 12:24:40 +0200

    [TASK] Adds image and abstract to news listing

--- Configuration/TypoScript/RenderingDefinitions/news_list.ts
+++ Configuration/TypoScript/RenderingDefinitions/news_list.ts
@@ -3,15 +3,24 @@ tt_content {
     news_list = FLUIDTEMPLATE
     news_list {
         templateName = NewsList
-        dataProcessing.10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
-        dataProcessing.10 {
-            table = pages
-            pidInList.field = pages
-            selectFields = pages.*
-            where = NOT hidden AND doktype = 12
-            languageField = sys_language_uid
-            orderBy = news_datetime DESC
-            as = news
+        dataProcessing {
+            10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+            10 {
+                table = pages
+                pidInList.field = pages
+                selectFields = pages.*
+                where = NOT hidden AND doktype = 12
+                languageField = sys_language_uid
+                orderBy = news_datetime DESC
+                as = news
+                dataProcessing {
+                    10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+                    10 {
+                        references.fieldName = media
+                        as = images
+                    }
+                }
+            }
         }
         settings {
             list {
