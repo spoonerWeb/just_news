14eec9b814dcd0be2392469ac0e25d8e819488c2
Thomas Löffler <loeffler@spooner-web.de>
2016-11-07 16:04:28 +0100

    [TASK] Gets news categories

--- Configuration/TypoScript/RenderingDefinitions/news_list.ts
+++ Configuration/TypoScript/RenderingDefinitions/news_list.ts
@@ -18,6 +18,16 @@ tt_content {
                         references.fieldName = media
                         as = images
                     }
+                    20 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+                    20 {
+                        table = sys_category
+                        pidInList = 10
+                        leftjoin = sys_category_record_mm ON sys_category_record_mm.uid_local = sys_category.uid
+                        where.field = uid
+                        where.wrap = sys_category_record_mm.uid_foreign = |
+                        begin = 0
+                        as = newsCategories
+                    }
                 }
             }
         }
