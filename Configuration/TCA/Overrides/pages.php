fd16756a222bb0bc21834e772ecd973d53eaa2f9
Thomas Löffler <loeffler@spooner-web.de>
2017-01-02 22:44:41 +0100

    [TASK] Renames TS file

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -56,7 +56,7 @@ $GLOBALS['TCA']['pages']['types'][$newsDoktype]['showitem'] = str_replace(
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'just_news',
     'Configuration/TypoScript/PageTS/news.ts',
-    'News pages'
+    'News pages (sysfolder)'
 );
 
 call_user_func(
