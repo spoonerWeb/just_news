a172dca961e9097ea2425af374a0ba1ca6e6c5d4
Thomas Löffler <loeffler@spooner-web.de>
2019-10-02 22:05:49 +0200

    [FEATURE] Add v10 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Thomas Löffler',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.4.0-9.5.99',
+            'typo3' => '9.4.0-10.9.99',
             'fluid_styled_content' => ''
         ],
         'conflicts' => [],
