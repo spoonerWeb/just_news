c557b70545d2d5c6ec972a3655de95e71226a845
Markus Hofmann <m.hofmann@web-vision.de>
2023-03-08 22:23:05 +0100

    [TASK] Move Icon registry to ext_localconf.php
    Icons should be registered in ext_localconf.php
    up to TYPO3 v10.

--- ext_tables.php
+++ ext_tables.php
@@ -23,15 +23,7 @@ $boot = function ($packageKey) {
         'allowedTables' => '*',
     ];
 
-    // Provide icon for page tree, list view, ... :
-    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class)
-        ->registerIcon(
-            'apps-pagetree-justnews',
-            TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            [
-                'source' => 'EXT:' . $packageKey . '/Resources/Public/Icons/NewsArticle.svg',
-            ]
-        );
+
 
 
 
