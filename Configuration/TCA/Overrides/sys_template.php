286c7bacef2ebba9419227205f7517606aa2388b
Markus Hofmann <m.hofmann@web-vision.de>
2023-03-08 22:11:20 +0100

    [TASK] Call Overrides in anonymous function
    Call registry of static TypoScript for sys_template in an
    anonymous function following best practices

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,6 +1,9 @@
 <?php
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'just_news',
-    'Configuration/TypoScript/',
-    'News with TypoScript'
-);
+
+(static function (): void {
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'just_news',
+        'Configuration/TypoScript/',
+        'News with TypoScript'
+    );
+})();
